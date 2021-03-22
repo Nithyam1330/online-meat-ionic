34bababca5cc190a09ffbfe8bcc7aa28457ed00c
Sai <sai@PurpleTalks-MacBook-Air.local>
2021-03-22 16:53:43 +0530

    First Commit

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,15 @@
+// Angular Moudles
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+// Custom Modules
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+  ]
+})
+
+// Import this Module in all submodules for reusablility
+export class SharedModule { }

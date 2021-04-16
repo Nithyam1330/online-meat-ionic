bd7adb283ffb6f58bb096ad88d9e331b613ca39a
Sai <sai@PurpleTalks-MacBook-Air.local>
2021-04-16 22:06:06 +0530

    Actions for address management UI

--- src/app/views/admin-views/profile-dashboard/address-management/address-management.component.ts
+++ src/app/views/admin-views/profile-dashboard/address-management/address-management.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { ActionSheetController } from '@ionic/angular';
 import { LoaderService } from 'src/app/shared/services/common/loader/loader.service';
 import { ToasterService } from 'src/app/shared/services/common/toaster/toaster.service';
 import { CommonRequestService } from 'src/app/shared/services/http/common-request.service';
@@ -17,7 +18,8 @@ export class AddressManagementComponent implements OnInit {
     private router: Router,
     private commonRequestService: CommonRequestService,
     private loaderService: LoaderService,
-    private toasterService: ToasterService
+    private toasterService: ToasterService,
+    private actionSheetController: ActionSheetController
   ) {}
 
   ngOnInit() {
@@ -32,6 +34,39 @@ export class AddressManagementComponent implements OnInit {
   public addAddress() {
     this.router.navigate(['profile', 'address', 'add-address']);
   }
+
+  async openAddressOptions() {
+    const actionSheet = await this.actionSheetController.create({
+      header: 'Actions',
+      cssClass: 'my-custom-class',
+      buttons: [{
+        text: 'Delete',
+        role: 'destructive',
+        icon: 'trash',
+        handler: () => {
+          console.log('Delete clicked');
+        }
+      }, {
+        text: 'Edit',
+        icon: 'Pencil',
+        handler: () => {
+          console.log('Share clicked');
+        }
+      }, {
+        text: 'Cancel',
+        icon: 'close',
+        role: 'cancel',
+        handler: () => {
+          console.log('Cancel clicked');
+        }
+      }]
+    });
+    await actionSheet.present();
+
+    const { role } = await actionSheet.onDidDismiss();
+    console.log('onDidDismiss resolved with role', role);
+  }
+
 
   /**Fetch User Addresses from DB */
   private async getAllAddress() {
